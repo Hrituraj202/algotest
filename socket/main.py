05a368454710ac6ee3a2ed90fa1820c3cc1b6055
Ritu Raj <ritu.raj@lenskart.in>
2023-08-16 01:51:49 +0530

    Added Broadcast to Socket

--- socket/main.py
+++ socket/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 app = FastAPI()
 
+clients = []
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    clients.append(websocket)
     await websocket.accept()
     while True:
         try:
             data = await websocket.receive_text()
             # print(data)
-            await websocket.send_text(f"{data}")
+            for client in clients:
+                if client != websocket:
+                    await client.send_text(f"{data}")
         except WebSocketDisconnect:
             pass
