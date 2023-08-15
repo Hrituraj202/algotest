5686f2db615207fa03a4017365d64243157ec4d4
Ritu Raj <ritu.raj@lenskart.in>
2023-08-16 04:12:40 +0530

    Added WS Client In Cron

--- cron/main.py
+++ cron/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from fastapi_utils.tasks import repeat_every
 
+from websocket import create_connection
+
 app = FastAPI()
 
 @app.on_event("startup")
 @repeat_every(seconds=30, wait_first=True)
-def periodic():
+async def periodic():
+    ws = create_connection("ws://socket:8000/ws")
+    ws.send("Hello, World")
+    ws.close()
     print("hi")
--- socket/main.py
+++ socket/main.py
@@ -6,13 +6,17 @@ clients = []
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     clients.append(websocket)
+    print(*clients)
     await websocket.accept()
     while True:
         try:
-            data = await websocket.receive_text()
+            try:
+                data = await websocket.receive_text()
+            except RuntimeError:
+                break
             # print(data)
             for client in clients:
                 if client != websocket:
                     await client.send_text(f"{data}")
         except WebSocketDisconnect:
-            pass
+            clients.remove(websocket)
